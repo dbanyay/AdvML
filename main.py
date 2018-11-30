fe39807f79255969845c2b1ab2f3016034b46fbb
dbanyay <banyaydani@gmail.com>
2018-11-30 10:27:36 +0100

    exp_kernel

--- main.py
+++ main.py
@@ -28,25 +28,69 @@ X = np.arange(-2, 2.2, 0.2)
 
 K = np.empty((length, length))
 for i in range(length):
-	for j in range(length):
-		K[i, j] = np.dot(X[i], X[j])
+    for j in range(length):
+        K[i, j] = np.dot(X[i], X[j]) # linear kernel
+
+# mu = np.zeros(length)
+#
+# # priors = np.random.multivariate_normal (mu ,K);
+#
+#
+# priors = st.multivariate_normal.rvs(mu, K, length)
+#
+#
+# for i in range(5):
+#     plt.plot(X, priors[i])
+# plt.show()
+
+x_obs = np.array([X[1],X[2]])
+y_obs = np.array([ti[1],ti[2]])
 
 
-priors = st.multivariate_normal.rvs(mean=[0] * length, cov=K, size=1000)
+N_obs = 2
+K_obs = np.empty((N_obs, N_obs))
+for i in range(N_obs):
+    for j in range(N_obs):
+        K_obs[i,j] = np.dot(x_obs[i], x_obs[j])
+
+K_obs_star = np.empty((N_obs, length))
+for i in range(N_obs):
+    for j in range(length):
+        K_obs_star[i,j] = np.dot(x_obs[i], X[j])
+
+post_mean = K_obs_star.T.dot(np.linalg.pinv(K_obs)).dot(y_obs)
+post_var = K - K_obs_star.T.dot(np.linalg.pinv(K_obs)).dot(K_obs_star)
+
+posteriors = st.multivariate_normal.rvs(mean=post_mean, cov=post_var, size = length)
+
+# for i in range(length):
+#     plt.plot(X, posteriors[i])
+
+# plt.plot(x_obs, y_obs, 'ro')
+# plt.show()
+#
+# plt.plot(X,ti)
+# plt.show()
+
+# use exponential kernel
+
+
+sigma_f = 0.3
+
+def exp_kernel(X,l,sigma_f):
+    K_exp = np.empty((length, length))
+    for i in range(length):
+        for j in range(length):
+            K_exp[i, j] = sigma_f ** 2 * np.exp(-(1.0 / (l ** 2)) * (X[i] - X[j]) ** 2)
+
+    return K_exp
+
+mu = np.zeros(length)
+
+# priors = np.random.multivariate_normal (mu ,K);
+priors_exp = st.multivariate_normal.rvs(mu, exp_kernel(X,1,0.3), length)
+
 
 for i in range(5):
-	plt.plot(X, priors[i])
-
-
+    plt.plot(X, priors_exp[i])
 plt.show()
-
-
-
-
-
-#pick 1 variable
-
-rand = np.random.randint(0, len(ti))
-pick = [xi[rand],ti[rand]]
-
-print(pick)
